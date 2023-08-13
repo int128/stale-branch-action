60299d6dec8ea15dfaa9f4542fbfea0b15d049a8
Hidetake Iwata <int128@gmail.com>
2023-08-13 17:44:42 +0900

    Ignore branch deletion error

--- src/run.ts
+++ src/run.ts
@@ -1,8 +1,8 @@
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
 import * as github from '@actions/github'
 import * as listRefs from './queries/listRefs'
 import { getStaleRefs } from './stale'
-import { deleteRefs } from './git'
 
 type Inputs = {
   refPrefix: string
@@ -23,16 +23,22 @@ export const run = async (inputs: Inputs): Promise<void> => {
   const expiration = new Date(Date.now() - inputs.expirationDays * 24 * 60 * 60 * 1000)
   core.info(`Expiration at ${expiration.toISOString()}`)
   const staleRefs = getStaleRefs(refs, inputs.refPrefix, expiration)
-  core.info(`Stale refs:\n${staleRefs.join('\n')}`)
   core.setOutput('stale-refs', staleRefs.join('\n'))
-
   if (staleRefs.length === 0) {
     core.info(`No stale branch`)
     return
   }
+  core.info(`Stale refs:\n${staleRefs.join('\n')}`)
+
   if (inputs.dryRun) {
     core.info(`Exiting due to dry-run`)
     return
   }
-  await deleteRefs(staleRefs)
+  for (const ref of staleRefs) {
+    // Ignore deletion error such as branch protection rule
+    const code = await exec.exec('git', ['push', 'origin', '--delete', ref], { ignoreReturnCode: true })
+    if (code !== 0) {
+      core.warning(`Failed to delete ${ref}`)
+    }
+  }
 }
